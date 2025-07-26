ce303ead3736cd0a03951c86caa1373af13bef7f
emareaitch <dev.emareaitch@gmail.com>
2025-07-26 17:49:49 +0530

    Responsive

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
 // This is where it all goes :)
+  const toggleBtn = document.querySelector('.menu-toggle');
+  const header = document.querySelector('header');
+
+  toggleBtn.addEventListener('click', () => {
+    header.classList.toggle('menu-open');
+  });
