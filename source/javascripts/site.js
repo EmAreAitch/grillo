28a5a37e5d33af36e3a00a5b0122f4858f4c1201
emareaitch <dev.emareaitch@gmail.com>
2025-07-31 13:00:59 +0530

    Updated date input

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -5,3 +5,6 @@
   toggleBtn.addEventListener('click', () => {
     header.classList.toggle('menu-open');
   });
+
+  const today = new Date().toISOString().split('T')[0];
+  document.getElementById('reservation-date').setAttribute('min', today);
